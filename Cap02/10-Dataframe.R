72dd6471a757d565515156fc9828c0ba58322594
GasparPSousa <gasparufrj@gmail.com>
2021-04-14 01:10:41 -0300

    DataFrames

--- Cap02/10-Dataframe.R
+++ Cap02/10-Dataframe.R
@@ -1,110 +1,154 @@
-vetor1 <- c(1:20)
-vetor1
-length(vetor1)
-mode(vetor1)
-class(vetor1)
-typeof(vetor1)
-matriz1 <- matrix(1:20, nrow = 2)
-matriz1
-length(matriz1)
-mode(matriz1)
-class(matriz1)
-typeof(matriz1)
-array1 <- array(1:5, dim = c(3, 3, 3))
-array1
-length(array1)
-mode(array1)
-class(array1)
-typeof(array1)
-View(iris)
-length(iris)
-mode(iris)
-class(iris)
-typeof(iris)
-lista1 <- list(a = matriz1, b = vetor1)
-lista1
-length(lista1)
-mode(lista1)
-class(lista1)
-typeof(lista1)
-func1 <- function(x) {
-var1 <- x * x
-return(var1)
-}
-func1(5)
-class(func1)
-objects()
-rm(array1, func1)
-objects()
-array1 <- array(1:5, dim = c(3, 3, 3))
-func1 <- function(x) {
-var1 <- x * x
-return(var1)
-}
-q()
+# DataFrames e Operações com DataFrames
+
+
+# Configurando o diretório de trabalho
+# Coloque entre aspas o diretório de trabalho que você está usando no seu computador
+# Não use diretórios com espaços no nome
+
 setwd("~/Cursos/DSA/FCD/BigDataRAzure/Cap02")
+
+# Para saber qual o diretório estou trabalhando:
 getwd()
+
+
+# Criando um dataframe vazio
+
 df <- data.frame()
 class(df)
 df
+
+
+# Criando vetores vazios
+
 nomes <- character()
 idades <- numeric()
 itens <- numeric()
 codigos <- integer()
+
 df <- data.frame(c(nomes, idades, itens, codigos))
-df <- data.frame()
-df <- data.frame(c(nomes, idades, itens, codigos))
-df <- data.frame(c(nomes, idades, itens, codigos))
-df <- data.frame(c(nomes, idades, itens, codigos))
+df
+
+
+# Criando vetores
+
 pais = c("Portugal", "Inglaterra", "Irlanda", "Egito", "Brasil")
 nome = c("Bruno", "Tiago", "Amanda", "Bianca", "Marta")
 altura = c(1.88, 1.76, 1.53, 1.69, 1.68)
 codigo = c(5001, 2183, 4702, 7965, 8890)
+
 df
+
+# Criando um dataframe de diversos vetores
+
 pesquisa = data.frame(pais, nome, altura, codigo)
 pesquisa
+
+
+
+# Adicionando um novo vetor a um dataframe existente
+
 olhos = c("verde", "azul", "azul", "castanho", "castanho")
 pesq = cbind(pesquisa, olhos)
 pesq
+
+
+
+
+# Informações sobre o dataframe
+
 str(pesq)
 dim(pesq)
 length(pesq)
+
+# Obtendo um vetor de um dataframe
+
 pesq$pais
 pesq$nome
 pesq$codigo
+
+
+# Extraindo um único valor - Fazer um Slicing(Fatiamento)
 pesq[1, 1]
 pesq[3, 2]
+
+# Número de Linhas e Colunas
+
 nrow(pesq)
 ncol(pesq)
+
+
+# Primeiros elementos do dataframe
+
 head(pesq)
 head(mtcars)
+
+
+
+
+# Últimos elementos do dataframe
+
 tail(pesq)
 tail(mtcars)
+
+
+# Data frames built-in do R
+
 ?mtcars
 mtcars
 View(mtcars)
-?mtcars
-pesq[altura < 1.60, ]
+
+
+
+# Filtro para um subset de dados que atendem a um critério
+
 pesq
 pesq[altura < 1.60, ]
 pesq[altura < 1.60, c('codigo', 'olhos')]
 pesq
+
+
+# Dataframes Nomeados
+
 names(pesq) <- c("País", "Nome", "Altura", "Código", "Olhos")
 pesq
+
+
 colnames(pesq) <- c("Var 1", "Var 2", "Var 3", "Var 4", "Var 5")
-pesq
 rownames(pesq) <- c("Obs 1", "Obs 2", "Obs 3", "Obs 4", "Obs 5")
 pesq
+
+
+# Carregando um arquivo csv
+
 ?read.csv
 pacientes <- data.frame(read.csv(file = 'pacientes.csv', header = TRUE, sep = ","))
+
+
+
+# Visualizando o dataset
 View(pacientes)
 head(pacientes)
 summary(pacientes)
-summary(pacientes)
+
+## summary é um resumo estatístico de cada uma das variáveis
+
+
+
+# Visualizando as variáveis
+
 pacientes$Diabete
 pacientes$Status
 pacientes$status
+
+# Histograma
 hist(pacientes$Idade)
+
+
+# Combinando dataframes
+
 dataset_final <- merge(pesq, pacientes)
 dataset_final
+
+
+# Sair
 q()
