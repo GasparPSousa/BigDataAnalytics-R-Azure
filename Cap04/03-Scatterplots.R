03eb43fe89ee74ef736aa26779fcef476a71246f
GasparPSousa <gasparufrj@gmail.com>
2021-04-21 10:43:20 -0300

    Construindo ScatterPlots

---
+++ Cap04/03-Scatterplots.R
@@ -0,0 +1,54 @@
+# Scatterplots
+
+# Configurando o diretório de trabalho
+# Coloque entre aspas o diretório de trabalho que você está usando no seu computador
+# Não use diretórios com espaço no nome
+setwd("~/Cursos/DSA/FCD/BigDataRAzure/Cap04")
+
+# Para saber qual diretório estou trabalhando
+getwd()
+
+
+# Lista de pacotes base carregados
+search()
+
+
+
+
+# Define os dados
+set.seed(67)
+x = rnorm(10, 5, 7)
+y = rpois(10, 7)
+z = rnorm(10, 6, 7)
+t = rpois(10, 9)
+
+
+pdf("Grafico3_ScatterPlot.pdf")
+
+# Cria o Plot
+plot(x, y, col = 123, pch = 10, main = "Multi Scatterplot", 
+     col.main = "red", cex.main = 1.5, xlab = "Variável Independente", 
+     ylab = "Variável Dependente")
+
+
+# Adiciona outros dados 
+points(z, t, col = "blue", pch = 4)
+
+
+
+# Adiciona outros dados 
+points(y, t, col = 777, pch = 9)
+
+
+
+# Cria legenda
+legend(-6, 5.9, legend = c("Nível 1", "Nível 2", "Nível 3"),
+       col = c(123, "blue", 777), pch = c(10, 4, 9),
+       cex = 0.65, bty = "n")
+
+
+dev.off()
+
+
+# Sair
+q()
