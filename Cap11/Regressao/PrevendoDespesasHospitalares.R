771b648fb431126727cc88786fa93de2a340c092
GasparPSousa <gasparufrj@gmail.com>
2021-05-17 17:06:24 -0300

    Avaliacao e Interpretacao do Modelo - Parte 1

--- Cap11/Regressao/PrevendoDespesasHospitalares.R
+++ Cap11/Regressao/PrevendoDespesasHospitalares.R
@@ -89,3 +89,56 @@ despesasteste <- read.csv("~/Cursos/DSA/FCD/Scripts/Arquivos-Cap11/Regressao/des
 View(despesasteste)
 previsao2 <- predict(modelo, despesasteste)
 View(previsao2)
+
+
+# Etapa 4: Avaliando a Performance do Modelo
+# Mais detalhes sobre o modelo
+
+summary(modelo)
+
+# ****************************************************
+# *** Estas informações abaixo é que farão de você ***
+# *** um verdadeiro conhecedor de Machine Learning ***
+# ****************************************************
+
+# Equação de Regressão
+# y = a + bx (simples)
+# y = a + b0x0 + b1x1 (múltipla)
+
+# Resíduos
+# Diferença entre os valores observados de uma variável e seus valores previstos
+# Seus resíduos devem se parecer com uma distribuição normal, o que indica
+# que a média entre os valores previstos e os valores observados é próximo de 0 (o que é bom)
+
+# Coeficiente - Intercept - a (alfa)
+# Valor de a na equação de regressão
+
+# Coeficientes - Nomes das variáveis - b (beta)
+# Valor de b na equação de regressão
+
+# Obs: A questão é que lm() ou summary() têm diferentes convenções de 
+# rotulagem para cada variável explicativa. 
+# Em vez de escrever slope_1, slope_2, .... 
+# Eles simplesmente usam o nome da variável em qualquer saída para 
+# indicar quais coeficientes pertencem a qual variável.
+
+# Erro Padrão
+# Medida de variabilidade na estimativa do coeficiente a (alfa). O ideal é que este valor 
+# seja menor que o valor do coeficiente, mas nem sempre isso irá ocorrer.
+
+# Asteriscos 
+# Os asteriscos representam os níveis de significância de acordo com o p-value.
+# Quanto mais estrelas, maior a significância.
+# Atenção --> Muitos astericos indicam que é improvável que não exista 
+# relacionamento entre as variáveis.
+
+# Valor t
+# Define se coeficiente da variável é significativo ou não para o modelo. 
+# Ele é usado para calcular o p-value e os níveis de significância.
+
+# p-value
+# O p-value representa a probabilidade que a variável não seja relevante. 
+# Deve ser o menor valor possível. 
+# Se este valor for realmente pequeno, o R irá mostrar o valor 
+# como notação científica
+
