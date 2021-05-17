d83f9d1565399ce4c4d21ef9436b82d2d0a4e4b0
GasparPSousa <gasparufrj@gmail.com>
2021-05-17 16:33:12 -0300

    Coleta de Dados e Analise Exploratoria

--- Cap11/Regressao/PrevendoDespesasHospitalares.R
+++ Cap11/Regressao/PrevendoDespesasHospitalares.R
@@ -20,3 +20,48 @@ search()
 # para um conjunto de pacientes espalhados por 4 regiões do Brasil.
 # Esse dataset possui 1.338 observações e 7 variáveis.
 
+# Etapa 1 - Coletando os dados
+despesas <- read.csv("~/Cursos/DSA/FCD/Scripts/Arquivos-Cap11/Regressao/despesas.csv")
+View(despesas)
+
+
+# Etapa 2: Explorando e Preparando os Dados
+# Visualizando as variáveis
+str(despesas)
+
+
+
+# Medias de Tendência Central da variável gastos
+summary(despesas$gastos)
+
+# Construindo um histograma
+hist(despesas$gastos)
+hist(despesas$gastos, main = 'Histograma', xlab = 'Gastos (R$)')
+
+# Tabela de contingência das regiões
+table(despesas$regiao)
+
+
+# Explorando relacionamento entre as variáveis: Matriz de Correlação
+cor(despesas[c('idade', 'bmi', 'filhos', 'gastos')])
+
+
+# Nenhuma das correlações na matriz é considerada forte, mas existem algumas associações interessantes. 
+# Por exemplo, a idade e o bmi (IMC) parecem ter uma correlação positiva fraca, o que significa que 
+# com o aumento da idade, a massa corporal tende a aumentar. Há também uma correlação positiva 
+# moderada entre a idade e os gastos, além do número de filhos e os gastos. Estas associações implicam 
+# que, à media que idade, massa corporal e número de filhos aumenta, o custo esperado do seguro saúde sobe. 
+
+# Visualizando relacionamento entre as variáveis: Scatterplot
+# Perceba que não existe um claro relacionamento entre as variáveis
+pairs(despesas[c('idade', 'bmi', 'filhos', 'gastos')])
+
+
+# Scatterplot Matrix
+install.packages("psych")
+library(psych)
+
+
+
+# Este gráfico fornece mais informações sobre o relacionamento entre as variáveis
+pairs.panels(despesas[c('idade', 'bmi', 'filhos', 'gastos')])
