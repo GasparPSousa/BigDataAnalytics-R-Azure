4495f091d30d25401f4b081b7fdbcc1b4c70383b
GasparPSousa <gasparufrj@gmail.com>
2021-05-16 21:49:34 -0300

    Analise Exploratoria de Dados para Variaveis Numericas

--- Cap11/02-Analise-Exploratoria-Dados.R
+++ Cap11/02-Analise-Exploratoria-Dados.R
@@ -28,3 +28,52 @@ str(carros)
 
 summary(carros$ano)
 summary(carros[c('preco', 'kilometragem')])
+
+##### Análise Exploratória de Dados Para Variáveis Numéricas ##### 
+
+# Usando as funções
+mean(carros$preco)
+median(carros$preco)
+quantile(carros$preco)
+quantile(carros$preco, probs = c(0.01, 0.99))
+quantile(carros$preco, seq(from = 0, to = 1, by = 0.20))
+IQR(carros$preco) # Diferença entre Q3 e Q1
+range(carros$preco)
+diff(range(carros$preco))
+
+
+# Plot
+
+# Boxplot
+# Leitura de Baixo para Cima - Q1, Q2 (Mediana) e Q3
+
+boxplot(carros$preco, main = "BoxPlot para os Preços Carros Usados", ylab  = "Preço (R$)")
+boxplot(carros$kilometragem, main = "BoxPlot para a km de Carros Usados ", ylab = "kilometragem (R$)")
+
+
+# Histograma
+# Indicam a frequência de valores dentro de cada bin (classe de valores)
+
+hist(carros$preco, main = "Histograma para os Preços Carros Usados", ylab = "Preço (R$)")
+hist(carros$kilometragem, main = "Histograma para a km de Carros Usados", ylab = "kilometragem (R$)")
+hist(carros$kilometragem, main = "Histograma para a km de Carros Usados", breaks = 5, ylab = "kilometragem (R$)")
+
+
+# Scatterplot Preço x Km
+# Usando o preço como variável dependente (y)
+
+plot(x = carros$kilometragem, y = carros$preco,
+     main = "Scatterplot - Preço x km",
+     xlab = "kilometragem",
+     ylab = "Preço (R$)")
+
+# Medidas de Dispersão
+# Ao interpretar a variância, números maiores indicam que 
+# os dados estão espalhados mais amplamente em torno da 
+# média. O desvio padrão indica, em média, a quantidade 
+# de cada valor diferente da média.
+
+var(carros$preco)
+sd(carros$preco)
+var(carros$kilometragem)
+sd(carros$kilometragem)
