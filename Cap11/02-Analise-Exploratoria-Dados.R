4d7f90c4d7c08229c008299e385166530b51f23c
GasparPSousa <gasparufrj@gmail.com>
2021-05-16 23:10:16 -0300

    Analise Exploratoria de Dados para Variaveis Categoricas

--- Cap11/02-Analise-Exploratoria-Dados.R
+++ Cap11/02-Analise-Exploratoria-Dados.R
@@ -77,3 +77,95 @@ var(carros$preco)
 sd(carros$preco)
 var(carros$kilometragem)
 sd(carros$kilometragem)
+
+##### Análise Exploratória de Dados Para Variáveis Categóricas ##### 
+
+# Criando tabelas de contingência - representam uma única variável categórica
+# Lista as categorias das variáveis nominais
+
+?table
+str(carros)
+table(carros$cor)
+table(carros$modelo)
+str(carros)
+
+
+# Calculando a proporção de cada categoria
+model_table <- table(carros$modelo)
+prop.table(model_table)
+
+# Arrendondando os valores
+model_table <- table(carros$modelo)
+model_table <- prop.table(model_table) * 100
+round(model_table, digits = 1)
+
+
+# Criando uma nova variável indicando cores conservadoras 
+# (que as pessoas compram com mais frequência)
+
+head(carros)
+carros$conserv <- carros$cor %in% c("Preto", "Cinza", "Prata", "Branco")
+head(carros)
+
+# Checando a variável
+table(carros$conserv)
+
+# Verificando o relacionamento entre 2 variáveis categóricas
+# Criando uma crosstable 
+# Tabelas de contingência fornecem uma maneira de exibir 
+# as frequências e frequências relativas de observações 
+# (lembra do capítulo de Estatística?), que são classificados 
+# de acordo com duas variáveis categóricas. Os elementos de 
+# uma categoria são exibidas através das colunas; 
+# os elementos de outra categoria são exibidas sobre as linhas.
+install.packages("gmodels")
+library(gmodels)
+?CrossTable
+CrossTable(x = carros$modelo, y = carros$conserv)
+
+
+
+## Teste do Qui-quadrado
+
+# Qui Quadrado, simbolizado por χ2 é um teste de 
+# hipóteses que se destina a encontrar um valor da 
+# dispersão para duas variáveis nominais, avaliando a 
+# associação existente entre variáveis qualitativas.
+
+# É um teste não paramétrico, ou seja, não depende dos 
+# parâmetros populacionais, como média e variância.
+
+# O princípio básico deste método é comparar proporções, 
+# isto é, as possíveis divergências entre as frequências 
+# observadas e esperadas para um certo evento.
+# Evidentemente, pode-se dizer que dois grupos se 
+# comportam de forma semelhante se as diferenças entre 
+# as frequências observadas e as esperadas em cada 
+# categoria forem muito pequenas, próximas a zero.
+
+# Ou seja, Se a probabilidade é muito baixa, ele fornece 
+# fortes evidências de que as duas variáveis estão 
+# associadas.
+
+CrossTable(x = carros$modelo, y = carros$conserv, chisq = T)
+chisq.test(x = carros$modelo, y = carros$conserv)
+
+
+
+# Trabalhamos com 2 hipóteses:
+# Hipótese nula: As frequências observadas não são diferentes das frequências esperadas.
+# Não existe diferença entre as frequências (contagens) dos grupos.
+# Portanto, não há associação entre os grupos
+
+# Hipótese alternativa: As frequências observadas são diferentes das frequências esperadas, 
+# portanto existe diferença entre as frequências.
+# Portanto, há associação entre os grupos.
+
+
+# Neste caso, o valor do Chi = 0.15  
+# E graus de liberdade (df) = 2
+# Portanto, não há associação entre os grupos
+# O valor alto do p-value confirma esta conclusão.
+
+# Sair
+q()
