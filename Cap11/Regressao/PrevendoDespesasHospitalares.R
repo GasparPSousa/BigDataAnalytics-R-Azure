98550044c1ed4a9d3d0021b7add1c596bec1dee8
GasparPSousa <gasparufrj@gmail.com>
2021-05-17 16:14:37 -0300

    Definindo o Problema

---
+++ Cap11/Regressao/PrevendoDespesasHospitalares.R
@@ -0,0 +1,22 @@
+# Machine Learning - Regressão 
+# Prevendo Despesas Hospitalares
+
+# Configurando o diretório de trabalho
+# Coloque entre aspas o diretório de trabalho que você está usando no seu computador
+# Não use diretórios com espaço no nome
+setwd("~/Cursos/DSA/FCD/BigDataRAzure/Cap11/Regressao")
+
+# Para saber qual diretório estou trabalhando
+getwd()
+
+
+# Lista de pacotes base carregados
+search()
+
+
+# Problema de Negócio: Previsão de Despesas Hospitalares
+
+# Para esta análise, vamos usar um conjunto de dados simulando despesas médicas hipotéticas 
+# para um conjunto de pacientes espalhados por 4 regiões do Brasil.
+# Esse dataset possui 1.338 observações e 7 variáveis.
+
