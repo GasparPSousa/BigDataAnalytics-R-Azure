9d2f49b5e48a5aebff55f54919b52f72a849597c
GasparPSousa <gasparufrj@gmail.com>
2021-05-18 08:36:11 -0300

    Coleta de Dados e Analise Exploratoria

--- Cap11/Classificacao/PrevendoOcorrenciaCancer.R
+++ Cap11/Classificacao/PrevendoOcorrenciaCancer.R
@@ -15,3 +15,16 @@ search()
 
 # Definição do Problema de Negócio: Previsão de Ocorrência de Câncer de Mama
 # http://archive.ics.uci.edu/ml/datasets/Breast+Cancer+Wisconsin+%28Diagnostic%29
+
+
+## Etapa 1 - Coletando os Dados
+
+# Os dados do câncer da mama incluem 569 observações de biópsias de câncer, 
+# cada um com 32 características (variáveis). Uma característica é um número de 
+# identificação (ID), outro é o diagnóstico de câncer, e 30 são medidas laboratoriais 
+# numéricas. O diagnóstico é codificado como "M" para indicar maligno ou "B" para 
+# indicar benigno.
+
+dados <- read.csv("~/Cursos/DSA/FCD/Scripts/Arquivos-Cap11/Classificacao/dataset.csv", stringsAsFactors = F)
+str(dados)
+View(dados)
