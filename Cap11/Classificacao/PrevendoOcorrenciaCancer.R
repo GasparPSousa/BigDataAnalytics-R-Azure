e5fc5ae97176dd7ec684f6a9e4d0a75fe1cbfabd
GasparPSousa <gasparufrj@gmail.com>
2021-05-18 08:26:47 -0300

    Definindo o Problema

---
+++ Cap11/Classificacao/PrevendoOcorrenciaCancer.R
@@ -0,0 +1,17 @@
+# Prevendo a Ocorrência de Câncer
+
+# Configurando o diretório de trabalho
+# Coloque entre aspas o diretório de trabalho que você está usando no seu computador
+# Não use diretórios com espaço no nome
+setwd("~/Cursos/DSA/FCD/BigDataRAzure/Cap11/Classificacao")
+
+# Para saber qual diretório estou trabalhando
+getwd()
+
+
+# Lista de pacotes base carregados
+search()
+
+
+# Definição do Problema de Negócio: Previsão de Ocorrência de Câncer de Mama
+# http://archive.ics.uci.edu/ml/datasets/Breast+Cancer+Wisconsin+%28Diagnostic%29
