edb91d93303c3b8dff03dbd24c0e0ad60ff8d0c2
GasparPSousa <gasparufrj@gmail.com>
2021-05-22 12:16:05 -0300

    Exercicio Cap11 - Parte2 - Treinando o modelo

--- Cap11/ExerciciosCap11_Parte2.R
+++ Cap11/ExerciciosCap11_Parte2.R
@@ -58,4 +58,45 @@ corrgram(df)
 corrgram(df, order = T, lower.panel = panel.shade,
          upper.panel = panel.pie, text.panel = panel.txt)
 
+# Criando um histograma
+ggplot(df, aes(x = G3)) + 
+  geom_histogram(bins = 20, alpha = 0.5, fill = 'blue') + 
+  theme_minimal()
 
+# Treinando e Interpretando o Modelo
+# Import Library
+install.packages("caTools")
+library(caTools)
+
+
+# Criando as amostras de forma randômica
+set.seed(101)
+?sample.split
+amostra <- sample.split(df$age, SplitRatio = 0.70)
+
+
+# ***** Treinamos nosso modelo nos dados de treino *****
+# *****   Fazemos as predições nos dados de teste  *****
+
+# Criando dados de treino - 70% dos dados
+treino = subset(df, amostra == T)
+
+
+# Criando dados de teste - 30% dos dados
+teste = subset(df, amostra == F)
+
+# Gerando o Modelo (Usando todos os atributos)
+modelo_v1 <- lm(G3 ~ ., treino)
+modelo_v2 <- lm(G3 ~ G1 + G2, treino)
+modelo_v3 <- lm(G3 ~ absences, treino)
+modelo_v4 <- lm(G3 ~ Medu, treino)
+
+# Interpretando o Modelo
+summary(modelo_v1)
+# 0.8616
+summary(modelo_v2)
+# 0.8211
+summary(modelo_v3)
+# 0.0002675
+summary(modelo_v4)
+# 0.06442
